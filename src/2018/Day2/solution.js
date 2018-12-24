fdec00c7553d43ee9299eb54fe41c91976d12c9b
Jorge Zapata Guridi <jorge.zapata@jazg.net>
2018-12-24 00:22:16 -0800

    Starting day 3

--- src/2018/Day2/solution.js
+++ src/2018/Day2/solution.js
@@ -1,18 +1,77 @@
 const fs = require('fs');
 
-function getCheckSum(error, data){
+function getCheckSum(err, data) {
     let boxes = data.split("\n");
-    let counts = {};
+    let count2 = 0;
+    let count3 = 0;
+    let checkSum;
 
-    for (let box of boxes) {
-        getMap()
-        countsOcurrences()
+    for(let box of boxes) {
+        let boxMap = getBoxCount(box);
+        if (hasXCount(boxMap, 2)) {
+            count2++;
+        }
+        if (hasXCount(boxMap, 3)) {
+            count3++;
+        }
     }
-    console.log(boxes);
+
+    checkSum = count2 * count3;
+    console.log(checkSum);
+}
+
+function getBoxCount(box){
+    let map = {};
+    for(let i = 0; i < box.length; i++) {
+        if (!map.hasOwnProperty(box[i])) {
+            map[box[i]] = 1;
+        } else {
+            map[box[i]]++;
+        }
+    }
+
+    return map;
+}
+
+function hasXCount(map, x) {
+    for (let k in map) {
+        if (map[k] === x ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function getSimilarBoxes(err, data) {
+    let boxes = data.split("\n");
+
+    for(let i = 0; i < boxes.length - 1; i++) {
+        for (let j = i + 1; j < boxes.length; j++) {
+            if (isSimilar(boxes[i], boxes[j])) {
+                console.log(boxes[i], boxes[j]);
+            }
+        }
+    }
+}
+
+function isSimilar(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    let diff = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] !== str2[i]) {
+            diff++;
+        }
+    }
+
+    return diff === 1 ? true : false;
 }
 
 function main(){
-    fs.readFile(`${__dirname}/input.txt`, 'utf8', getCheckSum);
+    fs.readFile(`${__dirname}/input.txt`, 'utf-8', getSimilarBoxes)
 }
 
 main();
--- src/2018/Day3/part1.js
+++ src/2018/Day3/part1.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
 
 function main(){
-    fs.readFile(`${__dirname}/input.txt`, 'utf8', parseClaims);
+    fs.readFile(`${__dirname}/input.txt`, 'utf-8', parseClaims)
 }
 
 function parseClaims(err, data) {
-    entries = data.split("\n");
-
-    for (let i = 0; i < 100; i++) {
-        printClaim(entries[i])
+    let claims = data.split("\n");
+    
+    for (let i = 0; i < claims; i++) {
+        
     }
+    // for (let i = 0; i < 100; i++) {
+    //     printClaim(claims[i]);
+    // }
 }
 
 function printClaim(claim) {
-
+    let exp = /#([0-9]*) @ ([0-9]*),([0-9]*): ([0-9]*)x([0-9]*)/
+    let [full, id, offsetX, offsetY, x, y] = claim.match(exp)
+    console.log (offsetX)
 }
+
 main();
