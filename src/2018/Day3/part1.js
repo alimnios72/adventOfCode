be9e8664b01e88d23b82d987daef8e2a22c515b8
Jorge Zapata Guridi <jorge.guridi@hulu.com>
2019-03-27 22:46:24 -0700

    Day 3

--- src/2018/Day3/part1.js
+++ src/2018/Day3/part1.js
@@ -1,24 +1,81 @@
 const fs = require('fs');
 
 function main(){
-    fs.readFile(`${__dirname}/input.txt`, 'utf-8', parseClaims)
+    fs.readFile(`${__dirname}/input.txt`, 'utf-8', readInput)
 }
 
-function parseClaims(err, data) {
+function readInput(err, data) {
     let claims = data.split("\n");
-    
-    for (let i = 0; i < claims; i++) {
-        
+    let fabric = createEmptyFabric();
+
+    for (let i = 0; i < claims.length; i++) {
+        let claim = parseClaim(claims[i]);
+        addClaim(fabric, claim);
     }
-    // for (let i = 0; i < 100; i++) {
-    //     printClaim(claims[i]);
-    // }
+    printFabric(fabric);
+    let overlap = getOverlapArea(fabric);
+    console.log(overlap);
 }
 
-function printClaim(claim) {
+function createEmptyFabric() {
+    const maxX = 1000;
+    const maxY = 1000;
+    let fabric = [];
+
+    for (let i = 0; i < maxX; i++) {
+        fabric[i] = []
+        for (let j = 0; j < maxY; j++) {
+            fabric[i][j] = ".";
+        }
+    }
+
+    return fabric;
+}
+
+function parseClaim(claim) {
     let exp = /#([0-9]*) @ ([0-9]*),([0-9]*): ([0-9]*)x([0-9]*)/
     let [full, id, offsetX, offsetY, x, y] = claim.match(exp)
-    console.log (offsetX)
+    return {
+        id: id,
+        offsetX: parseInt(offsetX),
+        offsetY: parseInt(offsetY),
+        x: parseInt(x),
+        y: parseInt(y)
+    }
+}
+
+function addClaim(fb, cl) {
+    console.log(cl.offsetX, cl.x)
+    for (let i = cl.offsetX; i < (cl.offsetX + cl.x); i++) {
+        for (let j = cl.offsetY; j < (cl.offsetY + cl.y); j++) {
+            if (fb[i][j] === "_") {
+                fb[i][j] = "X";
+            } else {
+                fb[i][j] = "_";
+            }
+        }
+    }
+}
+
+// ToDo: write to file
+function printFabric(fb) {
+    for (let i = 0; i < fb.length; i++) {
+        let line = fb[i].join("");
+        console.log(line)
+    }
+}
+
+function getOverlapArea(fb) {
+    let area = 0;
+    for (let i = 0; i < fb.length; i++) {
+        for (let j = 0; j < fb[i].length; j ++) {
+            if (fb[i][j] === "X") {
+                area++;
+            }
+        }
+    }
+
+    return area
 }
 
 main();
