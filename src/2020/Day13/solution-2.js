ec2d0bb57c75dfc92e720beb47c3664de616a7b8
Jorge Guridi <jorge.guridi@hulu.com>
2020-12-16 18:42:47 -0800

    Optimized part2 of Day 13 2020

--- src/2020/Day13/solution-2.js
+++ src/2020/Day13/solution-2.js
@@ -1,7 +1,6 @@
-const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const fs = require('fs');
 
-function shuttleSearch(data, min) {
+function shuttleSearch(err, data) {
     let input = data.split("\n");
     let buses = input[1].split(",")
     .map((a, idx) => {
@@ -13,34 +12,24 @@ function shuttleSearch(data, min) {
     })
     .filter(a => a != null);
 
-    let found = false;
-    let first = buses.shift();
-    let i = ((min + first.value) - (min % first.value)) - first.value;
-    while (!found) {
-        let count = 0;
-        i += first.value;
-        for (let j = 0; j < buses.length; j++) {
-            if (((i % buses[j].value) + buses[j].offset) === buses[j].value) {
-                count++;
-            } else {
+    let time = 0;
+    let acc = 1;
+    for (let bus of buses) {
+        for (let i = 0; i < bus.value; i++) {
+            let next = time + (i * acc);
+            if ((next + bus.offset) % bus.value === 0) {
+                time = next;
+                acc = acc * bus.value;
                 break;
             }
         }
-        if (count == buses.length) {
-            found = true;
-        }
-        
-        if (i > Number.MAX_SAFE_INTEGER) {
-            console.error("Result is larger than integer");
-            break;
-        }
     }
-    console.log(i);
+
+    console.log(time);
 }
 
 function main(){
-    // fs.readFile(`${__dirname}/input.txt`, 'utf8', (err, data) => shuttleSearch(data, 100000000000000));
-    fs.readFile(`${__dirname}/input1.txt`, 'utf8', (err, data) => shuttleSearch(data, 1));
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', shuttleSearch);
 }
 
 main();
