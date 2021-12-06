f3ad4eff7cb21ab39d9823d207c9cd2160cb1dfb
Jorge Zapata Guridi <jorge.zapata@jazg.net>
2021-12-05 19:09:23 -0800

    Cleaned up solution Day5 2021

--- src/2021/Day5/solution.js
+++ src/2021/Day5/solution.js
@@ -11,22 +11,17 @@ function findOverlaps(err, data) {
 
     for (let coord of coordinates) {
         let [[x1, y1], [x2, y2]] = coord;
+        let x = x1 === x2 ? 0 : (x2 - x1) / Math.abs(x2 - x1);
+        let y = y1 === y2 ? 0 : (y2 - y1) / Math.abs(y2 - y1);
 
-        if (x1 === x2) {
-            let start = y1 < y2 ? y1 : y2;
-            let end = y1 > y2 ? y1 : y2;
-            for (let x = start; x <= end; x++) {
-                updateCoord(grid, x, x1);
-            }
+      if (x1 === x2 || y1 === y2) {
+        while (x1 !== x2 + x || y1 !== y2 + y) {
+            updateCoord(grid, y1, x1);
+            x1 += x;
+            y1 += y;
         }
+      }
 
-        if (y1 === y2) {
-            let start = x1 < x2 ? x1 : x2;
-            let end = x1 > x2 ? x1 : x2;
-            for (let y = start; y <= end; y++) {
-                updateCoord(grid, y1, y);
-            }
-        }
     }
 
     for (let i = 0; i < grid.length; i++) {
@@ -37,7 +32,6 @@ function findOverlaps(err, data) {
         }
     }
 
-    // console.log(grid);
     console.log(pointOverlaps);
 }
 
@@ -51,27 +45,13 @@ function findOverlapsWithD(err, data) {
 
     for (let coord of coordinates) {
         let [[x1, y1], [x2, y2]] = coord;
+        let x = x1 === x2 ? 0 : (x2 - x1) / Math.abs(x2 - x1);
+        let y = y1 === y2 ? 0 : (y2 - y1) / Math.abs(y2 - y1);
 
-        if (x1 === x2) {
-            let start = y1 < y2 ? y1 : y2;
-            let end = y1 > y2 ? y1 : y2;
-            for (let x = start; x <= end; x++) {
-                updateCoord(grid, x, x1);
-            }
-        } else if (y1 === y2) {
-            let start = x1 < x2 ? x1 : x2;
-            let end = x1 > x2 ? x1 : x2;
-            for (let y = start; y <= end; y++) {
-                updateCoord(grid, y1, y);
-            }
-        } else {
-            let x = x1 < x2 ? 1 : -1;
-            let y = y1 < y2 ? 1 : -1;
-            while(x1 !== x2 + x && y1 !== y2 + y) {
-                updateCoord(grid, y1, x1);
-                x1 += x;
-                y1 += y;
-            }
+        while (x1 !== x2 + x || y1 !== y2 + y) {
+            updateCoord(grid, y1, x1);
+            x1 += x;
+            y1 += y;
         }
     }
 
@@ -83,10 +63,6 @@ function findOverlapsWithD(err, data) {
         }
     }
 
-    // let textGrid = grid.map((line) => line.join('')).reduce((acc, val) => {
-    //     return acc + val + '\n';
-    // }, '');
-    // console.log(textGrid);
     console.log(pointOverlaps);
 }
 
@@ -98,9 +74,16 @@ function updateCoord(grid, x, y) {
     }
 }
 
+function printGrid(grid) {
+    let textGrid = grid.map((line) => line.join('')).reduce((acc, val) => {
+        return acc + val + '\n';
+    }, '');
+    console.log(textGrid);
+}
+
 
 function main() {
-    // fs.readFile(`${__dirname}/input1.txt`, 'utf8', findOverlaps);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', findOverlaps);
     fs.readFile(`${__dirname}/input.txt`, 'utf8', findOverlapsWithD);
 }
 
