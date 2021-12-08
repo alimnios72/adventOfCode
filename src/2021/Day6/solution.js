abccb854eefe571f355d1ae7e55fa345b8c4de8d
Jorge Guridi <jorge.guridi@hulu.com>
2021-12-07 19:15:32 -0800

    Solution to Day 6 2021

--- src/2021/Day6/solution.js
+++ src/2021/Day6/solution.js
@@ -1,48 +1,47 @@
 const fs = require('fs');
-const DAYS = 18; //80
+const TIMER = 9;
 
-function lanternFishSimulator(err, data) {
+function part1(err, data) {
+    console.log(lanternFishSimulator(data, 80));
+}
+
+function part2(err, data) {
+    console.log(lanternFishSimulator(data, 256));
+}
+
+function lanternFishSimulator(data, days) {
     let initialState = data.split(',').map(Number);
     let simulation = {
-        prev: new Map(),
-        curr: new Map()
-    }
-
-    for (let i = 0; i < 9; i++) {
-        simulation.prev.set(i, 0);
-        simulation.curr.set(i, 0);
+        curr: new Map(Array.from(Array(TIMER), (_val, index) => [index, 0])),
+        next: new Map(Array.from(Array(TIMER), (_val, index) => [index, 0]))
     }
 
     for (let num of initialState) {
-        simulation.prev.set(num, simulation.prev.get(num) + 1);
+        simulation.curr.set(num, simulation.curr.get(num) + 1);
     }
 
-    for (let day = 0; day < DAYS; day++) {
-        for (let i = 8; i >= 0; i--) {
+    for (let day = 1; day <= days; day++) {
+        for (let i = TIMER - 1; i >= 0; i--) {
             if (i === 0) {
-                if (simulation.prev.get(i) > 0) {
-                    simulation.curr.set(6, simulation.prev.get(i));
-                    simulation.curr.set(8, simulation.prev.get(i));
+                if (simulation.curr.get(i) > 0) {
+                    simulation.next.set(6, simulation.next.get(6) + simulation.curr.get(i));
+                    simulation.next.set(8, simulation.next.get(8) + simulation.curr.get(i));
                 }
             } else {
-                simulation.curr.set(i - 1, simulation.curr.get(i - 1) + simulation.prev.get(i));
+                simulation.next.set(i - 1, simulation.next.get(i - 1) + simulation.curr.get(i));
             }
         }
-        simulation.prev = new Map(simulation.curr);
 
-        for (let i = 0; i < 9; i++) {
-            simulation.curr.set(i, 0);
-        }
+        simulation.curr = new Map(simulation.next);
+        simulation.next = new Map(Array.from(Array(TIMER), (_val, index) => [index, 0]));
     }
 
-    console.log(Object.values(simulation.prev).reduce((acc, val) => {
-        return acc + val;
-    }, 0));
+    return [ ...simulation.curr.values() ].reduce((acc, val) => acc + val, 0);
 }
 
 function main() {
-    fs.readFile(`${__dirname}/input1.txt`, 'utf8', lanternFishSimulator);
-    // fs.readFile(`${__dirname}/input.txt`, 'utf8', findOverlapsWithD);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part1);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
 }
 
 main();
