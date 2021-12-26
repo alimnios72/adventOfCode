d374ef8e5db7f405ddad6ab9418b58f0dfbbb95e
Jorge Zapata Guridi <jorge.zapata@jazg.net>
2021-12-25 18:05:09 -0800

    Solved part 2 of Day16 2021

--- src/2021/Day16/solution.js
+++ src/2021/Day16/solution.js
@@ -12,7 +12,18 @@ function part1(err, data) {
         .join('');
 
     let packages = readPackage(bitsTransmission, 0);
-    console.log(packages);
+    let sum = sumVersions(packages);
+    console.log(sum);
+}
+
+function part2(err, data) {
+    let bitsTransmission = data.trim().split('')
+    .map(char => HEX2BIN.get(char))
+    .join('');
+
+    let packages = readPackage(bitsTransmission, 0);
+    let value = getPackageValues(packages);
+    console.log(value);
 }
 
 function readPackage(str, start) {
@@ -29,19 +40,30 @@ function readPackage(str, start) {
         package.value = value;
         start = end;
     } else {
+        let subPackage = {};
         let lengthType = getNextNBits(str, start, 1);
         start++;
         let packagesLength = lengthType === '0' ? 15 : 11;
         let length = getNextNBits(str, start, packagesLength, true);
+        start += packagesLength;
 
         if (lengthType === '0') {
-            package.subpackages.push(readPackage(str, start));
+            let end = start + length;
+            while (start < end) {
+                subPackage = readPackage(str, start);
+                package.subpackages.push(subPackage);
+                start = subPackage.end;
+            }
         } else {
             for (let i = 0; i < length; i++) {
-                package.subpackages.push(readPackage(str, start))
+                subPackage = readPackage(str, start);
+                package.subpackages.push(subPackage);
+                start = subPackage.end;
             }
         }
     }
+
+    package.end = start;
 
     return package;
 }
@@ -70,29 +92,118 @@ function bits2Int(str) {
     return parseInt(str, 2);
 }
 
-/**
-{
-    version: number;
-    type: number;
-    isLiteral: boolean;
-    value?: number;
-    subPackets: [
-        {
-            version: number;
-            type: number;
-            isLiteral: boolean;
-            value?: number;
-        }
-    ]
-}
- */
+function sumVersions(package) {
+    let sum = 0;
+    let queue = [];
+    let curPackage;
 
-function part2(err, data) {
+    queue.push(package);
+
+    while (queue.length !== 0) {
+        curPackage = queue.shift();
+        sum += curPackage.version;
+
+        for (let subPackage of curPackage.subpackages) {
+            queue.push(subPackage);
+        }
+    }
+
+    return sum;
+}
+
+function getPackageValues(package) {
+    let first, second;
+
+    switch(package.type) {
+        case 0:
+            // sum package
+            let sum = 0;
+            for (let subpackage of package.subpackages){
+                if (subpackage.subpackages.length > 0) {
+                    sum += getPackageValues(subpackage);
+                } else {
+                    sum += subpackage.value;
+                }
+            }
+            return sum;
+        case 1:
+            // product package
+            let product = package.subpackages.length === 0 ? 0 : 1;
+            for (let subpackage of package.subpackages){
+                if (subpackage.subpackages.length > 0) {
+                    product *= getPackageValues(subpackage);
+                } else {
+                    product *= subpackage.value;
+                }
+            }
+            return product;
+        case 2:
+            // minimum package
+            let min = package.subpackages.length === 0 ? 0 : Infinity;
+            for (let subpackage of package.subpackages){
+                if (subpackage.subpackages.length > 0) {
+                    min = Math.min(min, getPackageValues(subpackage));
+                } else {
+                    min = Math.min(min, subpackage.value);
+                }
+            }
+            return min;
+        case 3:
+            // maximum package
+            let max = package.subpackages.length === 0 ? 0 : -Infinity;
+            for (let subpackage of package.subpackages){
+                if (subpackage.subpackages.length > 0) {
+                    max = Math.max(max, getPackageValues(subpackage));
+                } else {
+                    max = Math.max(max, subpackage.value);
+                }
+            }
+            return max;
+        case 5:
+            // greater than package
+            if (package.subpackages[0].subpackages.length > 0) {
+                first = getPackageValues(package.subpackages[0]);
+            } else {
+                first = package.subpackages[0].value;
+            }
+            if (package.subpackages[1].subpackages.length > 0) {
+                second = getPackageValues(package.subpackages[1]);
+            } else {
+                second = package.subpackages[1].value;
+            }
+            return first > second ? 1 : 0;
+        case 6:
+            // less than package
+            if (package.subpackages[0].subpackages.length > 0) {
+                first = getPackageValues(package.subpackages[0]);
+            } else {
+                first = package.subpackages[0].value;
+            }
+            if (package.subpackages[1].subpackages.length > 0) {
+                second = getPackageValues(package.subpackages[1]);
+            } else {
+                second = package.subpackages[1].value;
+            }
+            return first < second ? 1 : 0;
+        case 7:
+            // equal package
+            if (package.subpackages[0].subpackages.length > 0) {
+                first = getPackageValues(package.subpackages[0]);
+            } else {
+                first = package.subpackages[0].value;
+            }
+            if (package.subpackages[1].subpackages.length > 0) {
+                second = getPackageValues(package.subpackages[1]);
+            } else {
+                second = package.subpackages[1].value;
+            }
+            return first === second ? 1 : 0;
+    }
 }
 
 function main() {
-    fs.readFile(`${__dirname}/input1.txt`, 'utf8', part1);
-    // fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part1);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
 }
 
 main();
