41d1046d723d9fe9bf3d012da4437bea181d8558
Jorge Zapata Guridi <jorge.zapata@jazg.net>
2021-12-28 12:52:17 -0800

    Solved day 20 2021

--- src/2021/Day20/solution.js
+++ src/2021/Day20/solution.js
@@ -1,51 +1,47 @@
 const fs = require('fs');
-const ITERATIONS = 2;
 
 function part1(err, data) {
     let [ imageEnhancer, rawImage ] = data.split('\n\n');
     let image = rawImage.split('\n').map(line => line.split(''));
+    const ITERATIONS = 2;
 
     for (let it = 0; it < ITERATIONS; it++) {
-        image = expandImage(image, 4);
-        image = applyEnhancement(image, imageEnhancer);
+        image = applyEnhancement(image, imageEnhancer, it);
     }
 
     let litPixels = image.reduce((rowSum, row) => rowSum + row.reduce((colSum, val) => colSum + (val === '#' ? 1 : 0), 0), 0);
-    // for (let row of image) {
-    //     console.log(row.join(''))
-    // }
-
     console.log(litPixels)
 }
 
 function part2(err, data) {
-}
+    let [ imageEnhancer, rawImage ] = data.split('\n\n');
+    let image = rawImage.split('\n').map(line => line.split(''));
+    const ITERATIONS = 50;
 
-function expandImage(image, factor) {
-    let expandedImage = Array.from(Array(image.length + factor * 2), () => Array(image[0].length + factor * 2).fill('.'));
-
-    for (let i = factor; i < expandedImage.length - factor; i++) {
-        for (let j = factor; j < expandedImage[i].length - factor; j++) {
-            expandedImage[i][j] = image[i - factor][j - factor];
-        }
+    for (let it = 0; it < ITERATIONS; it++) {
+        image = applyEnhancement(image, imageEnhancer, it);
     }
 
-    return expandedImage;
+    let litPixels = image.reduce((rowSum, row) => rowSum + row.reduce((colSum, val) => colSum + (val === '#' ? 1 : 0), 0), 0);
+    console.log(litPixels)
 }
 
-function applyEnhancement(image, enhancer) {
-    let enhancedImage = image.map(row => row.slice());
+function applyEnhancement(image, enhancer, n) {
+    let enhancedImage = [];
 
-    for (let i = 0; i < image.length; i++) {
-        for (let j = 0; j < image[i].length; j++) {
-            enhancedImage[i][j] = getWindowValue(image, i, j, enhancer);
+    for (let i = -1; i < image.length + 1; i++) {
+        let enhancedRow = [];
+
+        for (let j = -1; j < image[0].length + 1; j++) {
+            enhancedRow.push(getWindowValue(image, n, i, j, enhancer));
         }
+        enhancedImage.push(enhancedRow);
     }
 
     return enhancedImage;
 }
 
-function getWindowValue(image, i, j, enhancer){
+function getWindowValue(image, n, i, j, enhancer){
     const window = [ [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1] ];
     let binaryStr = '', decValue;
 
@@ -54,7 +50,8 @@ function getWindowValue(image, i, j, enhancer){
         let newJ = j + coord[1];
 
         if (image[newI] == null || image[newI][newJ] == null) {
-            binaryStr += '0';
+            if (enhancer[0] === '#' && n % 2 === 1) binaryStr += '1';
+            else binaryStr += '0';
         } else {
             binaryStr += image[newI][newJ] === '.' ? '0' : '1';
         }
@@ -67,7 +64,7 @@ function getWindowValue(image, i, j, enhancer){
 
 function main() {
     fs.readFile(`${__dirname}/input.txt`, 'utf8', part1);
-    // fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
 }
 
 main();
