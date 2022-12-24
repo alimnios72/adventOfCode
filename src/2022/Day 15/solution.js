964799a67c950fb3e4588ad8588dfae492efae97
Jorge Guridi <jorge.guridi@hulu.com>
2022-12-24 00:32:09 -0800

    Day 15 2022 part2

--- src/2022/Day 15/solution.js
+++ src/2022/Day 15/solution.js
@@ -4,6 +4,32 @@ function manhattanDistance(p1, p2) {
     return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
 }
 
+function createSquares(origin, edge, divider) {
+    let squares = [];
+    let length = edge / divider;
+    for (let x = origin.x; x < origin.x + edge; x += length) {
+        for (let y = origin.y; y < origin.y + edge; y += length) {
+            squares.push({
+                p1: { x: x, y: y },
+                p2: { x: x + length - 1, y: y },
+                p3: { x: x, y: y + length - 1 },
+                p4: { x: x + length -1 , y: y + length - 1},
+            });
+        }
+    }
+
+    return squares;
+}
+
+function isSquareInRhomb(square, rhomb) {
+    for (let p in square) {
+        if (manhattanDistance(square[p], rhomb.s) > rhomb.d) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function part1(err, data) {
     let re = /^Sensor at x=(\-?\d+), y=(\-?\d+): closest beacon is at x=(\-?\d+), y=(\-?\d+)$/;
     let y = 2000000;
@@ -38,10 +64,10 @@ function part1(err, data) {
 }
 
 function part2(err, data) {
-    let positions = new Map();
-    let grid = Array.from(Array(20), () => new Array(20).fill('.'));
     let re = /^Sensor at x=(\-?\d+), y=(\-?\d+): closest beacon is at x=(\-?\d+), y=(\-?\d+)$/;
-    let map = data.split('\n')
+    let edge = 4000000;
+    let divider = 100;
+    let rhombs = data.split('\n')
         .map(line => {
             let [, xs, ys, xb, yb] = line.match(re);
             let info = {
@@ -52,42 +78,38 @@ function part2(err, data) {
                 ...info,
                 d: manhattanDistance(info.s, info.b)
             }
-        })
-        .forEach(line => {
-            for (let i = 0; i <= line.d; i++) {
-                let y = line.s.y + i;
-                for (let j = 0; j <= line.d - i; j++) {
-                    let left = (-1 * j) + line.s.x;
-                    let right = line.s.x + j;
-                    positions.set(`${left}-${y}`, 1);
-                    positions.set(`${right}-${y}`, 1);
+        });
+    let squares = [{
+        p1: { x: 0, y: 0 },
+        p2: { x: edge, y: 0 },
+        p3: { x: 0, y: edge },
+        p4: { x: edge, y: edge },
+    }];
+    let tmp = [];
+
+    while (true) {
+        squares.forEach(square => {
+            let subSquares = createSquares(square.p1, edge, edge > divider ? divider : 4);
+            subSquares.forEach(subSquare => {
+                if (!rhombs.some(rhomb => isSquareInRhomb(subSquare, rhomb))) {
+                    tmp.push(subSquare);
                 }
-            }
-            for (let i = 1; i <= line.d; i++) {
-                let y = (-1 * i) + line.s.y;
-                for (let j = 0; j <= line.d - i; j++) {
-                    let left = (-1 * j) + line.s.x;
-                    let right = line.s.x + j;
-                    positions.set(`${left}-${y}`, 1);
-                    positions.set(`${right}-${y}`, 1);
-                }
-            }
+            });
         });
 
-    for (let j = 0; j < grid.length; j++) {
-        for (let i = 0; i < grid[0].length; i++) {
-            let key = `${i}-${j}`;
-            if (positions.has(key)) {
-                grid[i][j] = '#';
-            }
-        }
+        if (tmp.length === 1) break;
+
+        squares = tmp;
+        tmp = [];
+        edge /= divider;
     }
-    console.log(grid.forEach(g => console.log(g.join(''))))
+    let res = tmp.pop();
+    console.log(BigInt(res.p1.x) * BigInt(4000000) + BigInt(res.p1.y));
 }
 
 function main() {
     // fs.readFile(`${__dirname}/input.txt`, 'utf8', part1);
-    fs.readFile(`${__dirname}/input1.txt`, 'utf8', part2);
+    fs.readFile(`${__dirname}/input.txt`, 'utf8', part2);
 }
 
 main();
